a4c2b26a334c18d5d745f13692086d8f469b3ee6
Aritra Biswas <pandalearnstocode@gmail.com>
2022-03-26 08:06:58 +0530

    read csv file as pandas dataframe.

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
-
-from fastapi import APIRouter, Depends
+import pandas as pd
+import os
+import io, base64
+from io import StringIO
+from fastapi import APIRouter, Depends, File, UploadFile, Form
 
 from app.lib.ml.mainmod import main_func as main_func_a
 from app.lib.de.mainmod import main_func as main_func_b
@@ -17,15 +20,29 @@ async def index() -> dict[str, str]:
     }
 
 
-@router.get("/api_a/{num}", tags=["api_a"])
+@router.get("/ml/{num}", tags=["ml"])
 async def view_a(
     num: int, auth: Depends = Depends(get_current_user),
 ) -> dict[str, int]:
     return main_func_a(num)
 
 
-@router.get("/api_b/{num}", tags=["api_b"])
+@router.get("/de/{num}", tags=["de"])
 async def view_b(
     num: int, auth: Depends = Depends(get_current_user),
 ) -> dict[str, int]:
     return main_func_b(num)
+
+
+@router.post('/uploadfile/')
+async def create_data_file(
+        experiment: str = Form(...),
+        file_type: str = Form(...),
+        file_id: str = Form(...),
+        data_file: UploadFile = File(...),
+        ):
+    pd.read_csv(StringIO(str(data_file.file.read(), 'utf-16')), encoding='utf-16')
+    return {'filename': data_file.filename, 
+            'experiment':experiment, 
+            'file_type': file_type, 
+            'file_id': file_id}
